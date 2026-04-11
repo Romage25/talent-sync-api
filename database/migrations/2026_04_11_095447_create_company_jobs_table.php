0aba62d1f579f9d16ef0d083007dd55a7deea1ad
Rodel Gerodias <rodelgerodias25@gmail.com>
2026-04-11 18:31:46 +0800

    feat: add creation migration of company_users table
    feat: add creation migration of recruiters table
    
    feat: add creation migration of applicants table
    
    feat: add creation migration of company_jobs table
    
    feat: add creation migration of skills table
    
    feat: add creation migration of job_skills table
    
    feat: add creation migration of user_skills table
    
    feat: add creation migration of applications table

---
+++ database/migrations/2026_04_11_095447_create_company_jobs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('company_jobs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('created_by')->constrained('users')->onDelete('cascade');
+            $table->foreignId('company_id')->constrained()->onDelete('cascade');
+            $table->foreignId('job_status_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->string('description');
+            $table->string('location');
+            $table->integer('salary_min')->nullable();
+            $table->integer('salary_max')->nullable();
+            $table->string('varchar');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('company_jobs');
+    }
+};
