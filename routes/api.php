f52b0a444e4d387f46d43481466fb1f995cc5c99
Rodel Gerodias <rodelgerodias25@gmail.com>
2026-05-13 19:00:49 +0800

    feat: use profile.complete middleware and add route for complete-profile

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum', 'profile.complete'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
     Route::get('/user', function (Request $request) {
@@ -18,3 +18,5 @@ Route::middleware('auth:sanctum')->group(function () {
 
     // Route::get('/user', fn(Request $request) => $request->user());
 });
+
+Route::middleware('auth:sanctum')->put('/complete-profile', [AuthController::class, 'completeProfile']);
