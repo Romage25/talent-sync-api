b24c072d749e49c6888bc3f973ea953988222c40
Rodel Gerodias <rodelgerodias25@gmail.com>
2026-05-15 09:57:40 +0800

    fix: move get user route to other route group to avoid profile.complete middleware
    feat: add complete-profile route

--- routes/api.php
+++ routes/api.php
@@ -10,13 +10,15 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware(['auth:sanctum', 'profile.complete'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
+    // Route::get('/user', fn(Request $request) => $request->user());
+});
+
+Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return response()->json([
             'user' => $request->user()
         ]);
     });
 
-    // Route::get('/user', fn(Request $request) => $request->user());
+    Route::put('/complete-profile', [AuthController::class, 'completeProfile']);
 });
-
-Route::middleware('auth:sanctum')->put('/complete-profile', [AuthController::class, 'completeProfile']);
